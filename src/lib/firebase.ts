c220f7fd600af0380392f0fcec1ada8b5f1c5e9b
thitiphum-bluesage <123978441+thitiphum-bluesage@users.noreply.github.com>
2023-02-20 21:23:20 +0700

    remove firebase config

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -10,14 +10,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyB8YlXYv7PDF1tPZ-C_lDKf4m0175ar5-k",
-  authDomain: "oujob-5c5ad.firebaseapp.com",
-  databaseURL: "https://oujob-5c5ad-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "oujob-5c5ad",
-  storageBucket: "oujob-5c5ad.appspot.com",
-  messagingSenderId: "132504882805",
-  appId: "1:132504882805:web:33cce795db4a4e2817323c",
-  measurementId: "G-FQ7FQ1CTGW"
+  xxx
 };
 
 // Initialize Firebase
